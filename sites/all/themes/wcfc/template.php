cc0ad6db4615dd0089a9a4dab973fdc56afaec59
Nick Hepner <nick@hepnermedia.com>
2013-02-14 07:47:34 -0500

    [#77] Numerous changes to code to move towards completion.

--- sites/all/themes/wcfc/template.php
+++ sites/all/themes/wcfc/template.php
@@ -10,3 +10,25 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+
+function wcfc_form_alter(&$form, &$form_state, $form_id) {
+  if ($form_id == 'search_block_form') {
+    $search_text = t("Enter Keywords...");
+    
+    $form['search_block_form']['#title'] = t('Search'); // Change the text on the label element
+    $form['search_block_form']['#title_display'] = 'invisible'; // Toggle label visibilty
+    $form['search_block_form']['#size'] = 25;  // define size of the textfield
+    $form['search_block_form']['#default_value'] = $search_text; // Set a default value for the textfield
+    $form['actions']['submit']['#value'] = t('GO!'); // Change the text on the submit button
+    $form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/search_icon.png');
+
+    // Add extra attributes to the text box
+    $form['search_block_form']['#attributes']['onblur'] = "if (this.value == '') {this.value = '".$search_text."';}";
+    $form['search_block_form']['#attributes']['onfocus'] = "if (this.value == '".$search_text."') {this.value = '';}";
+    // Prevent user from searching the default text
+    $form['#attributes']['onsubmit'] = "if(this.search_block_form.value=='".$search_text."'){ alert('No search terms entered.'); return false; }";
+
+    // Alternative (HTML5) placeholder attribute instead of using the javascript
+    $form['search_block_form']['#attributes']['placeholder'] = $search_text;
+  }
+} 
